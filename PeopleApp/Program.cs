e8f93a4e88d4565ce4c6cd8589afc2bd1ea8774d
JJ <jjtico@gmail.com>
2020-01-27 14:47:26 -0300

    Sync

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -18,11 +18,16 @@ namespace PeopleApp
             // call static method
             var baby2 = Person.Procreate(harry, jill);
 
+            // ca;; an operator
+            var baby3 = harry * mary;
+
             WriteLine($"{harry.Name} has {harry.Children.Count} children.");
             WriteLine($"{mary.Name} has {mary.Children.Count} children.");
             WriteLine($"{jill.Name} has {jill.Children.Count} children.");
 
             WriteLine(format: "{0}'s first child is named \"{1}\".", arg0: harry.Name, arg1: harry.Children[0].Name);
+
+            WriteLine($"5! is {Person.Factorial(5)}");
         }
     }
 }
